d4128259a39f7f83a97402bce2b4662d16a2e88b
Sameldon930 <1071119121@qq.com>
2018-11-03 12:29:26 +0800

    #zzs#授权登录

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,19 +1,48 @@
 Page({
   data:{
-
+    //切换授权头像的变量
+    authorized:false,
+    userInfo:null
   },
   onload(options){
-    //旧版
-    wx.getUserInfo({
-        success:data=>{
-            console.log(data)
-        }
-    })  
-    //弹窗询问是否授权
+
+    this.userAuthorized()
+  
 
   },
-  getUserInfo(event){
-    console.log(event);
+  //判断用户是否已经授权过
+  userAuthorized(){
+    wx.getSetting({
+      success:data=>{
+        //用户授权过
+        if(data.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:data=>{
+              this.setData({
+                authorized:true,//代表已经授权
+                userInfo:data.userInfo
+
+              })
+            }
+          })
+        }else{
+          console.log('err')
+        }
+
+      }
+    })
+  },
+  
+  onGetUserInfo(event){
+    const userInfo = event.detail.userInfo
+    if(userInfo){
+      this.setData({
+        userInfo:userInfo,
+        authorized:true
+      })
+    }
+    
   }
+
 
 })
